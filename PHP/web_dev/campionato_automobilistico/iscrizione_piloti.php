9795b6681d140f8a6dc81502517da7a98633f3f0
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-02-04 22:42:36 +0100

    .

--- PHP/web_dev/campionato_automobilistico/iscrizione_piloti.php
+++ PHP/web_dev/campionato_automobilistico/iscrizione_piloti.php
@@ -6,26 +6,43 @@ require './navbar.php';
 
 <form method="post" action="iscrizione_piloti.php">
     <div class="card">
-        <h1><?= $title ?></h1>
+        <h1>Dati pilota</h1>
 
         <br>
-        <label for="nome_pilota"><strong>Nome pilota</strong></label>
+        <label for="nome_pilota"><strong>Nome</strong></label>
         <textarea id="nome_pilota" name="nome_pilota" rows="1" placeholder="..." required></textarea>
         <hr>
 
         <br>
-        <label for="cognome_pilota"><strong>Cognome pilota</strong></label>
+        <label for="cognome_pilota"><strong>Cognome</strong></label>
         <textarea id="cognome_pilota" name="cognome_pilota" rows="1" placeholder="..." required></textarea>
         <hr>
 
         <br>
-        <label for="nazionalita_pilota"><strong>Nazionalità pilota</strong></label>
+        <label for="nazionalita_pilota"><strong>Nazionalità</strong></label>
         <textarea id="nazionalita_pilota" name="nazionalita_pilota" rows="1" placeholder="..." required></textarea>
         <hr>
 
         <br>
-        <label for="tempo_pilota"><strong>Tempo pilota</strong></label>
+        <label for="tempo_pilota"><strong>Tempo</strong></label>
         <textarea id="tempo_pilota" name="tempo_pilota" rows="1" placeholder="..." required></textarea>
+        <hr>
+
+    </div>
+
+    <br>
+
+    <div class="card">
+        <h1>Dati casa</h1>
+
+        <br>
+        <label for="nome_casa"><strong>Nome</strong></label>
+        <textarea id="nome_casa" name="nome_casa" rows="1" placeholder="..." required></textarea>
+        <hr>
+
+        <br>
+        <label for="colore_casa"><strong>Colore</strong></label>
+        <textarea id="colore_casa" name="colore_casa" rows="1" placeholder="..." required></textarea>
         <hr>
     </div>
 
