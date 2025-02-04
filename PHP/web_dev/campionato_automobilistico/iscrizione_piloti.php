863ad3aee13f61a11cc4c5f996a3b68e0fa443b6
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-02-04 15:53:40 +0100

    .

--- PHP/web_dev/campionato_automobilistico/iscrizione_piloti.php
+++ PHP/web_dev/campionato_automobilistico/iscrizione_piloti.php
@@ -2,31 +2,35 @@
 $title = 'Iscrizione piloti';
 
 require './navbar.php';
-require './footer.php';
-
 ?>
 
-<form method="post" action="./iscrizione_piloti.php">
+<form method="post" action="iscrizione_piloti.php">
     <div class="card">
-        <h1><?=$title?><h1>
-                <br>
-                <label for="nome_pilota"><strong>Nome pilota</strong></label>
-                <textarea id="nome_pilota" name="nome_pilota" rows="1" placeholder="..." required></textarea>
-                <br>
+        <h1><?= $title ?></h1>
 
-                <br>
-                <label for="cognome_pilota"><strong>Cognome pilota</strong></label>
-                <textarea id="cognome_pilota" name="cognome_pilota" rows="1" placeholder="..." required></textarea>
-                <br>
+        <br>
+        <label for="nome_pilota"><strong>Nome pilota</strong></label>
+        <textarea id="nome_pilota" name="nome_pilota" rows="1" placeholder="..." required></textarea>
+        <hr>
 
-                <br>
-                <label for="nazionalita_pilota"><strong>Nazionalità pilota</strong></label>
-                <textarea id="nazionalita_pilota" name="nazionalita_pilota" rows="1" placeholder="..." required></textarea>
-                <br>
+        <br>
+        <label for="cognome_pilota"><strong>Cognome pilota</strong></label>
+        <textarea id="cognome_pilota" name="cognome_pilota" rows="1" placeholder="..." required></textarea>
+        <hr>
 
-                <br>
-                <label for="tempo_pilota"><strong>Tempo pilota</strong></label>
-                <textarea id="tempo_pilota" name="tempo_pilota" rows="1" placeholder="..." required></textarea>
-                <br>
+        <br>
+        <label for="nazionalita_pilota"><strong>Nazionalità pilota</strong></label>
+        <textarea id="nazionalita_pilota" name="nazionalita_pilota" rows="1" placeholder="..." required></textarea>
+        <hr>
+
+        <br>
+        <label for="tempo_pilota"><strong>Tempo pilota</strong></label>
+        <textarea id="tempo_pilota" name="tempo_pilota" rows="1" placeholder="..." required></textarea>
+        <hr>
     </div>
+
+    <input type="submit" class="submit-button" value="ISCRIVI">
 </form>
+
+<?php require './footer.php';
+?>
