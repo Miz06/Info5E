ca10c7a90177cd304f4770c8ec6181d71c44bd5a
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-02-06 15:07:23 +0100

    .

--- SQL/campionato_automobilistico.sql
+++ SQL/campionato_automobilistico.sql
@@ -63,3 +63,32 @@ VALUES ('Charles', 'Leclerc', 'Monegasco', 5, 'Ferrari'),
        ('Max', 'Verstappen', 'Olandese', 55, 'Red Bull'),
        ('Lando', 'Norris', 'Inglese', 1, 'McLaren'),
        ('Fernando', 'Alonso', 'Spagnolo', 32, 'Aston Martin');
+
+INSERT INTO db_campionato_automobilistico.gare (data, luogo, tempo_veloce)
+VALUES ('2024-03-10 14:00:00', 'Monza', '01:20:34'),
+       ('2024-04-07 14:00:00', 'Silverstone', '01:25:6'),
+       ('2024-05-12 14:00:00', 'Monaco', '01:12:8'),
+       ('2024-06-16 14:00:00', 'Spa-Francorchamps', '01:45:12'),
+       ('2024-07-21 14:00:00', 'Suzuka', '01:30:46');
+
+INSERT INTO db_campionato_automobilistico.gareggiare (id_pilota, luogo_gara, data_gara, posizione)
+VALUES (1, 'Monza', '2024-03-10 14:00:00', 2),
+       (2, 'Monza', '2024-03-10 14:00:00', 1),
+       (3, 'Monza', '2024-03-10 14:00:00', 3),
+       (4, 'Silverstone', '2024-04-07 14:00:00', 2),
+       (5, 'Silverstone', '2024-04-07 14:00:00', 1),
+       (3, 'Silverstone', '2024-04-07 14:00:00', 3),
+       (1, 'Monaco', '2024-05-12 14:00:00', 1),
+       (2, 'Monaco', '2024-05-12 14:00:00', 3),
+       (5, 'Monaco', '2024-05-12 14:00:00', 2);
+
+INSERT INTO db_campionato_automobilistico.partecipare (nome_casa, luogo_gara, data_gara)
+VALUES ('Ferrari', 'Monza', '2024-03-10 14:00:00'),
+       ('Mercedes', 'Monza', '2024-03-10 14:00:00'),
+       ('Red Bull', 'Monza', '2024-03-10 14:00:00'),
+       ('McLaren', 'Silverstone', '2024-04-07 14:00:00'),
+       ('Aston Martin', 'Silverstone', '2024-04-07 14:00:00'),
+       ('Red Bull', 'Silverstone', '2024-04-07 14:00:00'),
+       ('Ferrari', 'Monaco', '2024-05-12 14:00:00'),
+       ('Mercedes', 'Monaco', '2024-05-12 14:00:00'),
+       ('Aston Martin', 'Monaco', '2024-05-12 14:00:00');
