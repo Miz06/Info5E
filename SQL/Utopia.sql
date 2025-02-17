c6e24640b606b8421cbc7e4461eebd3af629d854
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2025-02-17 12:13:17 +0100

    .

--- SQL/Utopia.sql
+++ SQL/Utopia.sql
@@ -3,11 +3,11 @@ use db_dinastia_sovrani;
 
 create table db_dinastia_sovrani.sovrani(
 	nome varchar(30) primary key,
-	immagine varchar(30),
-	inizio_regno date,
-	fine_regno date,
-	successore varchar(30),
-	predecessore varchar(30),
+	immagine varchar(30) not null,
+	inizio_regno date not null,
+	fine_regno date not null,
+	successore varchar(30) default '',
+	predecessore varchar(30) default '',
 	foreign key (successore) references db_dinastia_sovrani.sovrani (nome),
 	foreign key (predecessore) references db_dinastia_sovrani.sovrani (nome)
 );
