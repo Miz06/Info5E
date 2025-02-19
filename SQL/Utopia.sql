c691f186c02a15fd23077f54fae341e6f9ca615d
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-02-19 23:36:40 +0100

    utopia

--- SQL/Utopia.sql
+++ SQL/Utopia.sql
@@ -1,13 +1,17 @@
-create database db_dinastia_sovrani;
-use db_dinastia_sovrani;
+create
+database db_dinastia_sovrani;
+use
+db_dinastia_sovrani;
 
-create table db_dinastia_sovrani.sovrani(
-	nome varchar(30) primary key,
-	immagine varchar(30) not null,
-	inizio_regno date not null,
-	fine_regno date not null,
-	successore varchar(30) default '',
-	predecessore varchar(30) default '',
-	foreign key (successore) references db_dinastia_sovrani.sovrani (nome),
-	foreign key (predecessore) references db_dinastia_sovrani.sovrani (nome)
+create table db_dinastia_sovrani.sovrani
+(
+    nome         varchar(30) primary key,
+    immagine     varchar(30) default '',
+    inizio_regno date not null,
+    fine_regno   date not null,
+    successore   varchar(30) default '',
+    predecessore varchar(30) default ''
+    -- foreign key (successore) references db_dinastia_sovrani.sovrani (nome),
+    -- foreign key (predecessore) references db_dinastia_sovrani.sovrani (nome)
 );
+
