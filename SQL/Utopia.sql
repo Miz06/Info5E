9689d3c2ed9337b02207ddec1e8bf140b9a92def
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2025-02-20 09:21:32 +0100

    Update Utopia.sql

--- SQL/Utopia.sql
+++ SQL/Utopia.sql
@@ -7,8 +7,8 @@ create table db_dinastia_sovrani.sovrani
     immagine     varchar(30) default '',
     inizio_regno date not null,
     fine_regno   date not null,
-    successore   varchar(30) default null,
-    predecessore varchar(30) default null,
+    successore   varchar(30),
+    predecessore varchar(30),
     foreign key (successore) references db_dinastia_sovrani.sovrani (nome),
     foreign key (predecessore) references db_dinastia_sovrani.sovrani (nome)
 );
