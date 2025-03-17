3281ed34a855a524ed167351e70349a986d36468
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2025-03-17 13:21:41 +0100

    Add files via upload

---
+++ PHP/lezioni/sessions/sessionIndexOne.php
@@ -0,0 +1,25 @@
+<?php
+//SESSIONE = durante la sessione posso continuare a far anviare dati tra gli script ovvero fari interagire (https://www.w3schools.com/php/php_sessions.asp)
+//i dati di una sessione sono sempre memorizzati nel server e non nel browser A DIFFERENZA DEI COOKIE
+//chiudendo il browser si chiude anche la sessione
+
+session_start();
+$_SESSION['materia'] = 'informatica';
+$_SESSION['scuola'] = 'itis';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>home page</p>
+    <a href="sessionIndexTwo.php">SessionIndexTwo</a>
+</body>
+</html>
