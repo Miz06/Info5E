879583ad00a4dbddf32962183527d287ff38c789
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2025-03-21 12:50:37 +0100

    Delete PHP/web_dev/FastRoute directory

--- PHP/web_dev/FastRoute/pages/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = 'Home';
-require '../references/navbar.php';
-?>
-
-<p>Home</p>
-</body>
-</html>
--- PHP/web_dev/FastRoute/pages/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-$_SESSION['email'] = 'Ospite';
-setcookie('nav_color', 'black');
-header('location: home.php');
