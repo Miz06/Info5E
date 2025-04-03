d098ffc1f597afc1b3630236f7fd64d88f9fcd0f
Alessandro Mizzon <122207463+Miz06@users.noreply.github.com>
2025-04-03 08:21:52 +0100

    Delete PHP/web_dev/elettronica directory

--- PHP/web_dev/elettronica/pages/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = "Home";
-require '../references/navbar.php';
-?>
-
-<p>ciao</p>
-
-<?php require '../references/footer.php'?>
--- PHP/web_dev/elettronica/references/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_SESSION = [];
-session_destroy();
-header("Location: ../pages/account.php");
