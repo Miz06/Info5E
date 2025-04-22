dec2e6fb411b8b9778c1dc925517dfa6664d7954
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-04-22 22:35:22 +0200

    .

--- PHP/web_dev/registro_elettronico/references/db.sql
+++ PHP/web_dev/registro_elettronico/references/db.sql
@@ -25,7 +25,15 @@ create table db_registro.genitori(
                                      username varchar(30) primary key,
                                      password varchar(255),
                                      nome varchar(30),
-                                     cognome varchar(30),
-                                     username_studente varchar(30),
-                                     foreign key (username_studente) references db_registro.studenti(username)
+                                     cognome varchar(30)
 );
+
+create table db_registro.genitori_studenti(
+                                              username_genitore varchar(30),
+                                              username_studente varchar(30),
+                                              primary key (username_genitore, username_studente),
+                                              foreign key (username_genitore) references db_registro.genitori(username),
+                                              foreign key (username_studente) references db_registro.studenti(username)
+);
+
+
