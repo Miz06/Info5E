cbfb4da457f0a48ce7ae4984ac809b4422e19948
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-04-28 22:51:10 +0200

    basi artifex

--- PHP/web_dev/Artifex/references/db.sql
+++ PHP/web_dev/Artifex/references/db.sql
@@ -19,25 +19,25 @@ create table db_artifex.guide(
                                  luogo_nascita varchar(100)
 );
 
-create table dn_artifex.turisti(
+create table db_artifex.turisti(
                                    recapito varchar(100),
                                    nome varchar(100),
                                    nazionalita varchar(100),
-                                   email varchar(100),
+                                   email varchar(100) primary key,
                                    lingua_madre varchar(30),
                                    foreign key (lingua_madre) references db_artifex.lingue(nome)
 );
 
 create table db_artifex.eventi(
                                   data date,
-                                  ora_inizio datetime,
+                                  ora_inizio time,
                                   prezzo float,
                                   min_partecipanti int,
                                   max_partecipanti int,
                                   titolo_visita varchar(100),
                                   id_guida int,
-                                  foreign key (id_guida) references db_artifex.guide(id),
                                   primary key (data, ora_inizio, titolo_visita),
+                                  foreign key (id_guida) references db_artifex.guide(id),
                                   foreign key (titolo_visita) references db_artifex.visite(titolo)
 );
 
@@ -46,8 +46,8 @@ create table db_artifex.prenotare(
                                      ora_inizio time,
                                      titolo_visita varchar(100),
                                      email varchar(100),
-                                     primary key(data, ora_inizio, titolo_visita, email)
-                                         foreign key (data, ora_inizio, titolo_visita) references db_artifex.eventi(nome, ora_inizio, titolo_visita),
+                                     primary key(data, ora_inizio, titolo_visita, email),
+                                     foreign key (data, ora_inizio, titolo_visita) references db_artifex.eventi(data, ora_inizio, titolo_visita),
                                      foreign key (email) references db_artifex.turisti(email)
 );
 
@@ -55,6 +55,6 @@ create table db_artifex.conoscere(
                                      id_guida int,
                                      nome varchar(100),
                                      foreign key (id_guida) references db_artifex.guide(id),
-                                     foreign key (nome) references db_artifex.ligue(nome),
+                                     foreign key (nome) references db_artifex.lingue(nome)
 );
 
